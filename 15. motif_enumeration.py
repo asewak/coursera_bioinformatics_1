8e10245c299bc8a25f9b98662143c037bedc3a6b
asewak <asewak17@gmail.com>
2018-03-30 16:19:50 -0700

    required true / false

--- 15. motif_enumeration.py
+++ 15. motif_enumeration.py
@@ -16,47 +16,67 @@ def hamming_distance(p, q):
 def neigbours(pattern, d):
     unique_letters = 'ACGT'
     if d == 0:
-        return(pattern)
+        return([pattern])
     if len(pattern) == 1:
-        return({'A', 'C', 'G', 'T'})
-    neigbourhood = set()
+        return(['A', 'C', 'G', 'T'])
+    neigbourhood = []
     suffix_pattern = pattern[1:]
     suffix_neigbours = neigbours(suffix_pattern, d)
     for s in suffix_neigbours:
         if hamming_distance(suffix_pattern, s) < d:
             for x in unique_letters:
-                neigbourhood.add(x + s)
+                neigbourhood.append(x + s)
         else:
-            neigbourhood.add(pattern[0] + s)
+            neigbourhood.append(pattern[0] + s)
     return(neigbourhood)
-    
+
+            
 
 def motif_enumeration(dna, k, d):
     patterns = set()
-    dna_len = len(dna)
+    #dna_len = len(dna)
     for strand in dna:
         for i in range(0, len(strand) - k + 1):
             pattern = strand[i:i+k]
             neigbourhood = neigbours(pattern, d)
 
             for j in neigbourhood:
-                freq = [0] * dna_len
+                #freq = [0] * dna_len
+                #c = 0
+                pattern_appears = True
                 for l in dna:
-                    freq_idx = dna.index(l)
+                    match_in_dna = False
+                    #freq_idx = dna.index(l)
                     for m in range(0, len(l) - k + 1):
                         pattern_strand = l[m:m+k]
                         if hamming_distance(j, pattern_strand) <= d:
-                            freq[freq_idx] += 1
-            
-                
-                # that is, the pattern exists in every dna
-                if all(s >= 0 for s in freq):
+                            #freq[freq_idx] += 1
+                            #c += 1
+                            match_in_dna = True
+                            break
+                    if match_in_dna == False:
+                        pattern_appears = False
+                        break
+                # that is, the pattern exists in every dna strand
+                #if all(s > 0 for s in freq):
+                #if c == len(dna):
+                if pattern_appears:
                     patterns.add(j)
     return(patterns)
 
-dna = ['ATTTGGC', 'TGCCTTA', 'CGGTATC', 'GAAAATT']
-k = 3
-d = 1
-motif_enumeration(dna, k, d)
-for x in dna:
-    print(x)
+
+with open('C:/Users/ainesh.sewak/Downloads/dataset_156_8.txt') as f:
+    file = f.read().splitlines()
+
+
+
+dna = file[1:]
+k = int(file[0].split()[0])
+d = int(file[0].split()[1])
+
+motifs = motif_enumeration(dna, k, d)
+print(' '.join(map(str,motifs)))
+
+
+
+
